250a997dc641b775c7e1c0e59746584b8ba02dfe
Ugesh Kumar KS <ugesh262000@gmail.com>
2023-06-18 14:07:28 +0530

    [ ugesh - Welcome note ]

---
+++ Day-19-Practice-Problem-Email-User/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Day_19_Practice_Problem_Email_User
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(" Welcome to Email & User Registration");
+        }
+    }
+}
