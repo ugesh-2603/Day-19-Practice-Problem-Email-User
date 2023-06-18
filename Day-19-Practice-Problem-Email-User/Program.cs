fdb68e1eb60390b055ab818b9984cfe926c38dae
Ugesh Kumar KS <ugesh262000@gmail.com>
2023-06-18 14:16:52 +0530

    [ ugesh - UC1-First-Name ]

--- Day-19-Practice-Problem-Email-User/Program.cs
+++ Day-19-Practice-Problem-Email-User/Program.cs
@@ -1,10 +1,36 @@
-﻿namespace Day_19_Practice_Problem_Email_User
+﻿using System.Text.RegularExpressions;
+
+namespace Day_19_Practice_Problem_Email_User
 {
     internal class Program
     {
         static void Main(string[] args)
         {
             Console.WriteLine(" Welcome to Email & User Registration");
+
+            string firstName = GetUserInput();
+
+            if (IsValidFirstName(firstName))
+            {
+                Console.WriteLine("Valid first name.");
+            }
+            else
+            {
+                Console.WriteLine("Invalid first name.");
+            }
+        }
+
+        public static string GetUserInput()
+        {
+            Console.WriteLine("Enter a first name:");
+            return Console.ReadLine();
+        }
+
+        public static bool IsValidFirstName(string firstName)
+        {
+            string pattern = "^[A-Z][a-zA-Z]{2,}$";
+            return Regex.IsMatch(firstName, pattern);
         }
     }
+    
 }
